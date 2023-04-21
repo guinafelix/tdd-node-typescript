9ba5c4830bad7e2d83f5c3d89ad3b17ef08db708
guinafelix <oguilhermeleitefelix@gmail.com>
2023-04-21 09:53:36 -0300

    refactor: change models to sue Omit

--- src/domain/usecases/add-survey.ts
+++ src/domain/usecases/add-survey.ts
@@ -1,10 +1,6 @@
-import { SurveyAnswerModel } from '@/domain/models/survey'
+import { SurveyAnswerModel, SurveyModel } from '@/domain/models/survey'
 
-export interface AddSurveyModel {
-  question: string
-  answers: SurveyAnswerModel[]
-  date: Date
-}
+export interface AddSurveyModel extends Omit<SurveyModel, 'id'> {}
 
 export interface AddSurvey {
   add: (data: AddSurveyModel) => Promise<void>
