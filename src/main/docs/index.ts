18170f69d04eea616e82ddfb96f1887562e84731
guinafelix <oguilhermeleitefelix@gmail.com>
2023-07-07 18:31:33 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    description: 'API de estudo sobre TDD, Clean Architecture e SOLID aplicado ao Node.js',
+    version: '1.0.0'
+  }
+}
